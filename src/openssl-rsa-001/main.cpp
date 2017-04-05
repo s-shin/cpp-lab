2ade5e98c782b3cd94cee0f51fe3c39bf300a21a
Shintaro Seki <s2pch.luck@gmail.com>
2017-04-06 03:36:26 +0900

    Implements RSACryptor.

--- src/openssl-rsa-001/main.cpp
+++ src/openssl-rsa-001/main.cpp
@@ -4,172 +4,11 @@
 #include <vector>
 
 #include "openssl/err.h"
-#include "openssl/evp.h"
 #include "openssl/pem.h"
-#include "openssl/rsa.h"
 
-template <class Derived>
-class BaseEncryptor {
- public:
-  template <class Buffer>
-  bool Encrypt(const Buffer &in, Buffer &out) {
-    return static_cast<Derived *>(this)->Encrypt(in, out);
-  }
-};
+#include "rsa_cryptor.h"
 
-template <class Derived>
-class BaseDecryptor {
- public:
-  template <class Buffer>
-  bool Decrypt(const Buffer &in, Buffer &out) {
-    return static_cast<Derived *>(this)->Decrypt(in, out);
-  }
-};
-
-template <class Derived>
-class BaseCryptor : public BaseEncryptor<Derived>,
-                    public BaseDecryptor<Derived> {};
-
-class RSACryptor : public BaseCryptor<RSACryptor> {
- public:
-  const int kPaddingMode = RSA_PKCS1_OAEP_PADDING;
-  const int kPaddingSize = RSA_PKCS1_PADDING_SIZE;
-  const std::function<const EVP_MD *(void)> kDigestRoutine = EVP_sha256;
-
-  RSACryptor(const std::shared_ptr<EVP_PKEY> &pkey) : pkey_(pkey) {}
-
-  template <class Buffer>
-  bool Encrypt(const Buffer &in, Buffer &out) {
-    if (!pkey_) {
-      last_error_ = "Invalid pkey.";
-      return false;
-    }
-
-    // Check message size.
-    // max_message_size = key_size - padding_size
-    // padding_size =
-    //   | mLen <= k - 2hLen - 2
-    //       in OAEP by https://tools.ietf.org/html/rfc8017#section-7.1.1
-    //   | 11
-    //       in v1.5 by https://tools.ietf.org/html/rfc8017#section-7.2
-    int max_inbuf_size;
-    int key_size = RSA_size(EVP_PKEY_get1_RSA(pkey_.get()));
-    const EVP_MD *md;
-    if (kPaddingMode == RSA_PKCS1_OAEP_PADDING) {
-      md = kDigestRoutine();
-      int hash_size = EVP_MD_size(md);
-      max_inbuf_size = key_size - 2 * hash_size - 2;
-    } else if (kPaddingMode == RSA_PKCS1_PADDING) {
-      max_inbuf_size = key_size - RSA_PKCS1_PADDING_SIZE;
-    } else {
-      last_error_ = "Unknown padding mode.";
-      return false;
-    }
-
-    if (in.size() > max_inbuf_size) {
-      last_error_ = "Too large input message size.";
-      return false;
-    }
-
-    std::shared_ptr<EVP_PKEY_CTX> ctx(EVP_PKEY_CTX_new(pkey_.get(), nullptr),
-                                      EVP_PKEY_CTX_free);
-    if (!ctx) {
-      last_error_ = "EVP_PKEY_CTX_new failed.";
-      return false;
-    }
-    if (EVP_PKEY_encrypt_init(ctx.get()) <= 0) {
-      last_error_ = "EVP_PKEY_encrypt_init failed.";
-      return false;
-    }
-
-    if (EVP_PKEY_CTX_set_rsa_padding(ctx.get(), kPaddingMode) <= 0) {
-      last_error_ = "EVP_PKEY_CTX_set_rsa_padding failed.";
-      return false;
-    }
-    if (kPaddingMode == RSA_PKCS1_OAEP_PADDING) {
-      if (EVP_PKEY_CTX_set_rsa_oaep_md(ctx.get(), md) <= 0) {
-        last_error_ = "EVP_PKEY_CTX_set_rsa_oaep_md failed.";
-        return false;
-      }
-    }
-
-    size_t outlen;
-    if (EVP_PKEY_encrypt(ctx.get(), nullptr, &outlen,
-                         reinterpret_cast<const unsigned char *>(in.data()),
-                         in.size()) <= 0) {
-      last_error_ = "EVP_PKEY_encrypt to obtain output buffer size failed.";
-      return false;
-    }
-    out.resize(outlen);
-    if (EVP_PKEY_encrypt(ctx.get(), reinterpret_cast<unsigned char *>(&out[0]),
-                         &outlen,
-                         reinterpret_cast<const unsigned char *>(in.data()),
-                         in.size()) <= 0) {
-      last_error_ = "EVP_PKEY_encrypt to encrypt input buffer size failed.";
-      return false;
-    }
-    out.resize(outlen);
-
-    return true;
-  }
-
-  template <class Buffer>
-  bool Decrypt(const Buffer &in, Buffer &out) {
-    if (!pkey_) {
-      return false;
-    }
-
-    // Check message size.
-    int max_inbuf_size = RSA_size(EVP_PKEY_get1_RSA(pkey_.get()));
-    if (in.size() > max_inbuf_size) {
-      return false;
-    }
-
-    std::shared_ptr<EVP_PKEY_CTX> ctx(EVP_PKEY_CTX_new(pkey_.get(), nullptr),
-                                      EVP_PKEY_CTX_free);
-    if (!ctx) {
-      return false;
-    }
-    if (EVP_PKEY_decrypt_init(ctx.get()) <= 0) {
-      return false;
-    }
-
-    if (EVP_PKEY_CTX_set_rsa_padding(ctx.get(), kPaddingMode) <= 0) {
-      return false;
-    }
-    if (kPaddingMode == RSA_PKCS1_OAEP_PADDING) {
-      if (EVP_PKEY_CTX_set_rsa_oaep_md(ctx.get(), kDigestRoutine()) <= 0) {
-        last_error_ = "EVP_PKEY_CTX_set_rsa_oaep_md failed.";
-        return false;
-      }
-    }
-
-    size_t outlen;
-    if (EVP_PKEY_decrypt(ctx.get(), nullptr, &outlen,
-                         reinterpret_cast<const unsigned char *>(in.data()),
-                         in.size()) <= 0) {
-      return false;
-    }
-    out.resize(outlen);
-    if (EVP_PKEY_decrypt(ctx.get(), reinterpret_cast<unsigned char *>(&out[0]),
-                         &outlen,
-                         reinterpret_cast<const unsigned char *>(in.data()),
-                         in.size()) <= 0) {
-      return false;
-    }
-    out.resize(outlen);
-
-    return true;
-  }
-
-  const std::string &last_error() { return last_error_; }
-
- private:
-  std::shared_ptr<EVP_PKEY> pkey_;
-  std::string last_error_;
-};
-
-inline void PrintOpenSSLError() {
+void PrintOpenSSLError() {
   std::shared_ptr<BIO> bio(BIO_new_file("/dev/stdout", "w"), BIO_free);
   if (!bio) {
     return;
@@ -220,63 +59,86 @@ int main(int argc, char **argv) {
 
   OpenSSL_add_all_algorithms();
 
-  std::shared_ptr<EVP_PKEY> public_pkey(nullptr, EVP_PKEY_free);
+  rsa::RSAPtr public_rsa;
   {
     std::shared_ptr<BIO> bio(BIO_new_mem_buf(public_key.c_str(), -1), BIO_free);
-    public_pkey.reset(
-        PEM_read_bio_PUBKEY(bio.get(), nullptr, nullptr, nullptr));
-    if (!public_pkey) {
+    public_rsa = rsa::Wrap(
+        PEM_read_bio_RSA_PUBKEY(bio.get(), nullptr, nullptr, nullptr));
+    if (!public_rsa) {
       std::cout << "Failed to parse public key." << std::endl;
       return 1;
     }
   }
-  std::shared_ptr<EVP_PKEY> private_pkey(nullptr, EVP_PKEY_free);
+  rsa::RSAPtr private_rsa;
   {
     std::shared_ptr<BIO> bio(BIO_new_mem_buf(private_key.c_str(), -1),
                              BIO_free);
-    private_pkey.reset(PEM_read_bio_PrivateKey(
+    private_rsa = rsa::Wrap(PEM_read_bio_RSAPrivateKey(
         bio.get(), nullptr, nullptr,
         const_cast<char *>(private_key_passphrase.c_str())));
-    if (!private_pkey) {
+    if (!private_rsa) {
       std::cout << "Failed to parse private key." << std::endl;
       return 1;
     }
   }
 
-  RSACryptor public_key_cryptor(public_pkey);
-  RSACryptor private_key_cryptor(private_pkey);
+  rsa::RSACryptor public_key_cryptor(
+      rsa::Key(public_rsa, rsa::Key::Type::kPublic));
+  rsa::RSACryptor private_key_cryptor(
+      rsa::Key(private_rsa, rsa::Key::Type::kPrivate));
 
   std::string input;
   std::cin >> input;
 
   {
-    std::string encrypted, decrypted;
-    if (!public_key_cryptor.Encrypt(input, encrypted)) {
+    std::string encrypted(public_key_cryptor.GetOutputBufferSizeForEncryption(),
+                          0);
+    int encrypted_size = public_key_cryptor.Encrypt(input, encrypted);
+    if (encrypted_size < 0) {
       std::cout << "Failed to encrypt by public key: "
                 << public_key_cryptor.last_error() << std::endl;
       PrintOpenSSLError();
       return 1;
     }
-    if (!private_key_cryptor.Decrypt(encrypted, decrypted)) {
-      std::cout << "Failed to decrypt by private key:"
-                << public_key_cryptor.last_error() << std::endl;
+    encrypted.resize(encrypted_size);
+
+    std::string decrypted(
+        private_key_cryptor.GetOutputBufferSizeForDecryption(), 0);
+    int decrypted_size = private_key_cryptor.Decrypt(encrypted, decrypted);
+    if (decrypted_size < 0) {
+      std::cout << "Failed to decrypt by private key: "
+                << private_key_cryptor.last_error() << std::endl;
+      PrintOpenSSLError();
       return 1;
     }
+    decrypted.resize(decrypted_size);
+
     std::cout << "pub enc -> prv dec => " << decrypted << std::endl;
   }
 
   {
-    std::string encrypted, decrypted;
-    if (!private_key_cryptor.Encrypt(input, encrypted)) {
-      std::cout << "Failed to encrypt by private key:"
-                << public_key_cryptor.last_error() << std::endl;
+    std::string encrypted(
+        private_key_cryptor.GetOutputBufferSizeForEncryption(), 0);
+    int encrypted_size = private_key_cryptor.Encrypt(input, encrypted);
+    if (encrypted_size < 0) {
+      std::cout << "Failed to encrypt by private key: "
+                << private_key_cryptor.last_error() << std::endl;
+      PrintOpenSSLError();
       return 1;
     }
-    if (!public_key_cryptor.Decrypt(encrypted, decrypted)) {
-      std::cout << "Failed to decrypt by public key:"
+    encrypted.resize(encrypted_size);
+
+    std::string decrypted(public_key_cryptor.GetOutputBufferSizeForDecryption(),
+                          0);
+    int decrypted_size = public_key_cryptor.Decrypt(encrypted, decrypted);
+    if (decrypted_size < 0) {
+      std::cout << "Failed to decrypt by public key: "
                 << public_key_cryptor.last_error() << std::endl;
+      PrintOpenSSLError();
       return 1;
     }
+    decrypted.resize(decrypted_size);
+
     std::cout << "prv enc -> pub dec => " << decrypted << std::endl;
   }
 
---
+++ src/openssl-rsa-001/rsa_cryptor.h
@@ -0,0 +1,191 @@
+#ifndef RSA_CRYPTOR_H_
+#define RSA_CRYPTOR_H_
+
+#include <memory>
+#include <string>
+
+#include "openssl/evp.h"
+#include "openssl/rsa.h"
+
+namespace rsa {
+
+template <class T>
+inline typename T::size_type container_sizeof(T v) {
+  return sizeof(typename T::value_type) * v.size();
+}
+
+using RSAPtr = std::shared_ptr<RSA>;
+
+inline RSAPtr Wrap(RSA *rsa) {
+  std::shared_ptr<RSA> p(rsa, RSA_free);
+  return p;
+}
+
+enum class PaddingMode {
+  // OAEP is unavailable for both encryption by private key and decryption by
+  // public key because OpenSSL is not supported.
+  // In the background, encryption by private key and decryption by public key
+  // are only used for not encryption but signing.
+  kOAEP = RSA_PKCS1_OAEP_PADDING,
+  kV1_5 = RSA_PKCS1_PADDING,
+};
+
+struct CryptoOption {
+  PaddingMode padding_mode;
+  const EVP_MD *md;
+};
+
+constexpr CryptoOption DefaultCryptoOption() {
+  return {PaddingMode::kV1_5, nullptr};
+}
+
+int GetPaddingSize(const CryptoOption &opt) {
+  switch (opt.padding_mode) {
+    case PaddingMode::kOAEP: {
+      // https://tools.ietf.org/html/rfc8017#section-7.1.1
+      const EVP_MD *md = opt.md;
+      if (md == nullptr) {
+        md = EVP_sha1();
+      }
+      int md_size = EVP_MD_size(md);
+      return md_size * 2 + 2;
+    }
+    case PaddingMode::kV1_5: {
+      // https://tools.ietf.org/html/rfc8017#section-7.2
+      return RSA_PKCS1_PADDING_SIZE;
+    }
+  }
+  return -1;
+}
+
+class Key {
+ public:
+  enum class Type { kPublic, kPrivate };
+
+  Key(RSAPtr rsa, Type type) : rsa_(rsa), type_(type) {}
+
+  int size() const { return RSA_size(rsa_.get()); }
+  Type type() const { return type_; }
+
+  explicit operator bool() const { return !!rsa_; }
+  operator RSA *() const { return rsa_.get(); }
+
+ private:
+  RSAPtr rsa_;
+  Type type_;
+};
+
+class RSACryptor {
+ public:
+  RSACryptor(Key key) : RSACryptor(std::move(key), DefaultCryptoOption()) {}
+
+  RSACryptor(Key key, CryptoOption option)
+      : key_(std::move(key)), option_(std::move(option)) {}
+
+  int GetMaxEncryptableBufferSize() {
+    if (!key_) {
+      last_error_ = "Invalid key.";
+      return -1;
+    }
+    return key_.size() - GetPaddingSize(option_);
+  }
+
+  int GetMaxDecryptableBufferSize() {
+    if (!key_) {
+      last_error_ = "Invalid key.";
+      return -1;
+    }
+    return key_.size();
+  }
+
+  int GetOutputBufferSizeForEncryption() { return key_.size(); }
+
+  int GetOutputBufferSizeForDecryption() { return key_.size(); }
+
+  template <class SrcBuffer, class DstBuffer>
+  int Encrypt(const SrcBuffer &src, DstBuffer &dst) {
+    return Encrypt(
+        reinterpret_cast<const unsigned char *>(&src[0]), container_sizeof(src),
+        reinterpret_cast<unsigned char *>(&dst[0]), container_sizeof(dst));
+  }
+
+  int Encrypt(const unsigned char *src_buf, int src_buf_size,
+              unsigned char *dst_buf, int dst_buf_size) {
+    if (!key_) {
+      last_error_ = "Invalid key.";
+      return -1;
+    }
+    if (src_buf_size > GetMaxEncryptableBufferSize()) {
+      last_error_ = "Too large input buffer size.";
+      return -1;
+    }
+    if (dst_buf_size < GetOutputBufferSizeForEncryption()) {
+      last_error_ = "Too small output buffer size.";
+      return -1;
+    }
+    int encrypted_size;
+    if (key_.type() == Key::Type::kPrivate) {
+      encrypted_size =
+          RSA_private_encrypt(src_buf_size, src_buf, dst_buf, key_,
+                              static_cast<int>(option_.padding_mode));
+    } else {
+      encrypted_size =
+          RSA_public_encrypt(src_buf_size, src_buf, dst_buf, key_,
+                             static_cast<int>(option_.padding_mode));
+    }
+    if (encrypted_size == -1) {
+      last_error_ = "Failed to encrypt.";
+      return -1;
+    }
+    return encrypted_size;
+  }
+
+  template <class SrcBuffer, class DstBuffer>
+  int Decrypt(const SrcBuffer &src, DstBuffer &dst) {
+    return Decrypt(
+        reinterpret_cast<const unsigned char *>(&src[0]), container_sizeof(src),
+        reinterpret_cast<unsigned char *>(&dst[0]), container_sizeof(dst));
+  }
+
+  int Decrypt(const unsigned char *src_buf, int src_buf_size,
+              unsigned char *dst_buf, int dst_buf_size) {
+    if (!key_) {
+      last_error_ = "Invalid key.";
+      return -1;
+    }
+    if (src_buf_size > GetMaxDecryptableBufferSize()) {
+      last_error_ = "Too large input buffer size.";
+      return -1;
+    }
+    if (dst_buf_size < GetOutputBufferSizeForDecryption()) {
+      last_error_ = "Too small output buffer size.";
+      return -1;
+    }
+    int decrypted_size;
+    if (key_.type() == Key::Type::kPrivate) {
+      decrypted_size =
+          RSA_private_decrypt(src_buf_size, src_buf, dst_buf, key_,
+                              static_cast<int>(option_.padding_mode));
+    } else {
+      decrypted_size =
+          RSA_public_decrypt(src_buf_size, src_buf, dst_buf, key_,
+                             static_cast<int>(option_.padding_mode));
+    }
+    if (decrypted_size == -1) {
+      last_error_ = "Failed to decrypt.";
+      return -1;
+    }
+    return decrypted_size;
+  }
+
+  const std::string &last_error() const { return last_error_; }
+
+ private:
+  Key key_;
+  CryptoOption option_;
+  std::string last_error_;
+};
+
+}  // namespace rsa
+
+#endif  // RSA_CRYPTOR_H_
